170dc5a9b20ef9850b0660f3d6f7d5fe9df83485
sainithinkatta <sainithinkatta24@gmail.com>
2024-11-08 15:19:34 -0600

    removed unused modules

--- src/components/Modules/Module03.jsx
+++
@@ -1,9 +0,0 @@
-const Module03 = () => (
-    <div>
-      <h2>Module 03 Content</h2>
-      <p>This is the content for Module 03.</p>
-    </div>
-  );
-  
-  export default Module03;
-  
--- src/components/Modules/Module05.jsx
+++
@@ -1,9 +0,0 @@
-const Module05 = () => (
-    <div>
-      <h2>Module 05 Content</h2>
-      <p>This is the content for Module 05.</p>
-    </div>
-  );
-  
-export default Module05;
-  
