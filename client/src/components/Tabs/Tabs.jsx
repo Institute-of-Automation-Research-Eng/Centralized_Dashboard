dfde3239472411e3822afc9e866799c9272ccae3
sainithinkatta <sainithinkatta24@gmail.com>
2024-11-15 11:29:05 -0600

    fixed tabs naming issue

--- client/src/components/Tabs/Tabs.jsx
+++ client/src/components/Tabs/Tabs.jsx
@@ -1,6 +1,10 @@
 import './Tabs.css';
 
 const Tabs = ({ activeTab, setActiveTab, tabOptions, isSidebar }) => {
+  const formatTabName = (tabName) => {
+    return tabName.replace(/([a-z])([A-Z])/g, '$1 $2');
+  };
+
   return (
     <div className={`tabs ${isSidebar ? 'sidebar' : 'horizontal'}`}>
       {tabOptions.map((tab, index) => (
@@ -9,7 +13,7 @@ const Tabs = ({ activeTab, setActiveTab, tabOptions, isSidebar }) => {
           className={`tab ${activeTab === tab ? 'active' : ''}`}
           onClick={() => setActiveTab(tab)}
         >
-          {tab}
+          {formatTabName(tab)}
         </button>
       ))}
     </div>
